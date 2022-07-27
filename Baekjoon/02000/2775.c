3c7fcbafd8ef19689f621d2807510da1bc960cec
JunBumHan <2hanjunbum6@gmail.com>
2022-07-27 15:57:52 +0900

    [2022/7/27] [PM 3:24]

--- Baekjoon/02000/2775.c
+++ Baekjoon/02000/2775.c
@@ -27,3 +27,33 @@ int main(void)
     //for(int i = 0; i < 15; ++i) printf("n_[%d] : %d\n",i,n_[i]);
     
 }
+
+
+/*
+재귀함수 풀이
+
+
+#include <stdio.h>
+
+int getNum(int k, int n)
+{
+    if( n == 1 ) return 1;
+    if( k == 0 ) return n;
+
+    return (getNum(k-1, n) + getNum(k, n-1));
+}
+
+int main()
+{
+    int t,k,n;
+
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d %d",&k,&n);
+        printf("%d\n",getNum(k,n));
+    }
+}
+
+
+*/
